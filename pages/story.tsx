59f8683f3c5a66b4f7ccb4748c6529b700c10a38
fiscayquimica <fisicayquimica@telefonica.net>
2023-03-22 00:45:21 +0100

    ok

--- pages/story.tsx
+++ pages/story.tsx
@@ -16,21 +16,21 @@ const Story = () => (
 			<div className='mt-2'>
 				<p className='text-zinc-600 dark:text-zinc-400'>
 					En los diez últimos años se ha avanzado bastante en la elaboración del hidromiel debido a un cambio en el uso de nutrientes que hace que se obtenga
-					 un buen hidromiel de forma rápida y perfectamente repetible cuando se hacen bien los procedimientos. La utilización de amortiguadores de pH durante la fermentación y la estabilización 
-					 y  la adición de estabilizadores una vez terminada la fermentación para prevenir fermentaciones indeseadas y la oxigenación hacen que el producto final tenga unas características 
+					 un buen hidromiel de forma rápida y perfectamente repetible cuando se hacen bien los procedimientos. La utilización de amortiguadores de pH durante la fermentación,  la 
+					 adición de estabilizadores una vez terminada la misma para prevenir tanto las fermentaciones indeseadas como la oxigenación hacen que el producto final tenga unas características 
 					 organolépticas que redunda en una mejora del producto final.
 					 A la hora de hacer un determinado hidromiel hay que partir de una receta donde se muestre el procedimiento a seguir así como los ingredientes. La variedad 
-					 de hidromieles es bastante grande y atiende a los ingredientes utilizados en el que se incluye la levadura que puede limitar el grado alcohólico obtenido, la graducación 
-					 alcohólica deseada y si queremos el hidromiel seco, semiseco, dulce o muy dulce. Es importante también la desinfección de todo lo que esté en contacto con el mosto o la hidromiel. 
-					 Después de la mezcla de la miel  y el agua, y eventualmente otros productos (manzana, otro tipo de fruta, té, café, ciertas especias) se le añade carbonato de potasio para que no camie mucho el pH 
-					 durante la fermentación. Se le añade la levadura previa preparación, distinta segúan sea 
-					 una levadura sólida o líquida. La fermentación es mejor seguirla mediante la utilización de un hidrómero o densímetro. Es muy importante la adición de nutrientes para 
-					 que el trabajo de las levaduras sea eficiente. La adición de nuitriente puede ser de una sola vez o de forma escalonada (en el día cero, 2, y 4) dependiendo de la gravedad 
+					 de hidromieles es bastante grande y atiende a los ingredientes utilizados (agua y miel principamente) en el que se incluye la levadura que puede limitar el grado alcohólico obtenido, la graduación 
+					 alcohólica deseada y el grado de dulzor, si es que lo queremos más o menos duce, o si lo queremos seco. Es importante también la desinfección de todo lo que esté en contacto con el mosto o la hidromiel. 
+					 Después de la mezcla de la miel  y el agua, y eventualmente otros productos (manzana, otro tipo de fruta, té, café, ciertas especias, etc. ) se le añade carbonato de potasio para que no cambie mucho el pH 
+					 durante la fermentación. Se le añade después la levadura preparación, distinta según sea 
+					 una levadura sólida o líquida. La fermentación es mejor seguirla mediante la utilización de un hidrómetro o densímetro. Es muy importante la adición de nutrientes para 
+					 que el trabajo de las levaduras sea eficiente. La adición de nuitrientes puede ser de una sola vez o de forma escalonada (en el día cero, 2, y 4) dependiendo de la gravedad 
 					 inicial de la hidromiel. Después de la fermentación se trasvasa a otro recipiente para separar las lías (resto de levaduras) del hidromiel. El hidromiel en estas 
-					 condiciones está turbio. Se puede acelerar el proceso de clarificación mediante lo que se llama choque en frío, la adición de sustancias clarificante o bien dejar reposar 
-					 durante un tiempo. Antes de clarificar podemos estabilizar  el mosto y endulzarlo a nuesto  (opcional). Una vez clarificado se envasa en botellas desinfectadas y de distinto tipo 
-					 dependiendo de si el hidromiel está estabilizado o no. Si no está estabilizado puede en ciertos casos, como que no se haya alcanzado la gravedad esperada o que se le añada 
-					 un azúcar de cebado para gasificarla, producir gases que hagan explotar la botellas a menos que la botella sea resitente y el tapón sea el adecuado.
+					 condiciones está turbio. Se puede acelerar el proceso de clarificación mediante lo que se llama choque en frío, la adición de sustancias clarificantes o bien dejarlo reposar 
+					 durante un tiempo. Antes de clarificar podemos estabilizar el hidromiel y endulzarlo a nuesto  (opcional). Una vez clarificado se envasa en botellas desinfectadas y de distinto tipo 
+					 dependiendo de si el hidromiel está estabilizado o no. Si no está estabilizado, puede en ciertos casos, como que no se haya alcanzado la gravedad esperada o que se le añada 
+					 un azúcar de cebado para gasificarla, que se reactive la fermentación y los gases producidos puede hacer que exploten las botellas a menos que  sea resitente y el tapón sea el adecuado.
 				
 
 				</p>
