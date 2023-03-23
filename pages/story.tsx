2b6599615000f822361b797eb27802c6a9a27561
fiscayquimica <fisicayquimica@telefonica.net>
2023-03-24 00:53:28 +0100

    ok

--- pages/story.tsx
+++ pages/story.tsx
@@ -113,9 +113,9 @@ const Story = () => (
 
 					</span>{' '}
 					<br/>
-					<div style="centered">
-						g de levadura x 1,25 (= g de GoFerm)x 25 = mL de agua necesaria 
-						</div> 
+					$(document).ready(function(){
+    $('#wrap').after("<div align='center'>g de levadura x 1,25 (= g de GoFerm)x 25 = mL de agua necesaria</div>");
+}); 
 					</li>
 					<li>
 					<span className='text-zinc-600 dark:text-zinc-400'>
