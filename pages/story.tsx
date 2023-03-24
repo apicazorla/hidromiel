b0d553236b15c4e07683ef6194467bd2f1fa66e4
fiscayquimica <fisicayquimica@telefonica.net>
2023-03-24 23:47:42 +0100

    ok

--- pages/story.tsx
+++ pages/story.tsx
@@ -177,7 +177,7 @@ const Story = () => (
 						Nos sirve más bien para ver la evolución de la fermentación y cuándo termina, por el valor constante de la gravedad. Tiene la ventaja de que sólo  se 
 						necesita una gota para medir la gravedad pero tiene el inconveniente de que el valor de la gravedad se ve influenciado cuando empieza a producirse alcohol. Para ello, 
 						existen calculadoras que corrigen la presencia de alcohol.		
-			
+			</li>
 				  </ul>
 
 
